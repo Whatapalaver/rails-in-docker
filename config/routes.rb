1f849194a2b3023fbce5c7fb4570a1bd6a475327
Angela Wolff <warriorwomenblog@gmail.com>
2021-05-01 17:41:55 +0100

    Add admin index page as root to reveal admin resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
+    get 'home/index'
     resources :articles
     resources :events
   end
 
-  root to: 'admin/articles#index'
+  root to: 'admin/home#index'
 end
